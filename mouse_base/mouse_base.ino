2f6abd5ea8ddfe11907ea6f79bf672d459192d70
Sharkywrecks <sharkywrecks@gmail.com>
2023-11-15 14:43:13 +0000

    Test

--- mouse_base/mouse_base.ino
+++ mouse_base/mouse_base.ino
@@ -38,7 +38,7 @@ void motor_loop()
     
   addressable_led[0] = CRGB::Green;
   FastLED.show();
-  mouse.run_motors(64,64);
+  mouse.run_motors(-255,255);//mouse.run_motors(64,64);
   delay(500);
 
   mouse.run_motors(0,0);
@@ -48,25 +48,52 @@ void motor_loop()
   
   addressable_led[0] = CRGB::Red;
   FastLED.show();
-  mouse.run_motors(-64,-64);
+  mouse.run_motors(255,-255);//mouse.run_motors(-64,-64);
   delay(500);
 
   mouse.run_motors(0,0);
   addressable_led[0] = CRGB::Black;
   FastLED.show();
   delay(2000);
-  Serial.println("Hello");
 }
 
 void setup() { 
     // configure the addressable led
     FastLED.addLeds<WS2812B, LED_PIN, GRB>(addressable_led, 1);
 
-
     Serial.begin(9600);
 }
-
+int before = 0,after = 0;
 void loop()
 {
-  motor_loop();
+  //sensor_loop();
+  //motor_loop();
+  test();
+}
+
+void test()
+{
+  mouse.enable_ir(2);
+  delay(10);
+  int LS = analogRead(SEN_LS);
+  int RS = analogRead(SEN_RS);
+  mouse.disable_ir(2);
+  mouse.enable_ir(1);
+  delay(10);
+  int turn = analogRead(SEN_LF);
+  mouse.disable_ir(1);
+  Serial.println(turn);
+  
+  int var = 64 + (LS - RS)*2;
+  
+  if(turn>130){
+    //Reverse, then turn
+    mouse.run_motors(-64,64);
+    delay(100);
+    mouse.run_motors(-64,-64);
+    delay(200);
+  }else{
+    //Forward
+    mouse.run_motors(64,-var);
+  }
 }
