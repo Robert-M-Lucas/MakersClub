0ec5c00542ba631c0a32ab791ae74c17ce9820e3
robert-m-lucas <noreply@example.com>
2023-11-22 13:59:39 +0000

    Implemented MouseIR

--- mouse_base/MouseIR.h
+++ mouse_base/MouseIR.h
@@ -1,9 +1,33 @@
 #ifndef MouseIR_h
-#define Mouse_h
+#define MouseIR_h
 
-class MouseIR {
+enum SensorDirection {
+    Forward = 1,
+    Side = 2,
+    Angle = 3
+};
+
+class IRReading
+{
+public:
+    unsigned left;
+    unsigned leftAngled;
+    unsigned leftForward;
+
+    unsigned right;
+    unsigned rightAngled;
+    unsigned rightForward;
+
+    static IRReading difference(IRReading before, IRReading after);
+};
+
+
+namespace MouseIR {
     void enable_ir(SensorDirection direction);
-    void disable_ir(int direction);
+    void disable_ir(SensorDirection direction);
+
+    IRReading read_all_ir();
+    IRReading read_all_callibrated();
 };
 
 #endif
